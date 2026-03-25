f141073f3e8b3ef1f39ba30411f2b2d63f03aa6d
Samuel Lindão Saboroso <malakaigamer5@gmail.com>
2026-03-24 22:03:11 -0300

    Tela login - com erro para entregar

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,25 @@
 import './style.css'
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const emailInput = document.querySelector("#email");
+    const senhaInput = document.querySelector("#senha");
+    const emailErro = document.querySelector("#emailErro");
+    const senhaErro = document.querySelector("#senhaErro");
+
+    emailErro.textContent = "";
+    senhaErro.textContent = "";
+
+    if (emailInput.value == "") {
+        emailErro.textContent = "Email é obrigatório";
+        return;
+    }
+
+    if (senhaInput.value == "") {
+        senhaErro.textContent = "Senha é obrigatória";
+        return;
+    }
+
+    window.location.href='index.html';
+});
