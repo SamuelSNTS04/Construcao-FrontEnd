a8f40b61a857e8b7dd293c62b2fc3b3877da1e2e
SamuelSNTS04 <malakaigamer5@gmail.com>
2026-06-17 22:04:24 -0300

    feat: conclui pratica08. Fecha #7

--- praticas/aluno-online-react/eslint.config.js
+++ praticas/aluno-online-react/eslint.config.js
@@ -1,29 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwind from "@tailwindcss/vite";
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwind()],
+  // Configuração do Servidor para Docker
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    watch: {
+      usePolling: true,
     },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    hmr: {
+      clientPort: 5173,
     },
   },
-])
+});
